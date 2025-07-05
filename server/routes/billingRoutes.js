13dc7c036decbb299a701ced4a14310aacaeeff2
Müberra Kurt <muberrakurt@ogr.eskisehir.edu.tr>
2025-07-05 23:00:38 +0300

    fix: Fix billing routes to handle missing user ID

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,7 +3,7 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-  apps.post('/api/stripe', requireLogin, async (req,res) => {
+  app.post('/api/stripe', requireLogin, async (req,res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
