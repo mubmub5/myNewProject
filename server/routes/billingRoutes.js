bfe5235ee1f7b3b4231958079876897f400ded70
Müberra Kurt <muberrakurt@ogr.eskisehir.edu.tr>
2025-07-05 22:58:08 +0300

    fix

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,7 +2,7 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin');
 
-module.exports = apps => {
+module.exports = app => {
   apps.post('/api/stripe', requireLogin, async (req,res) => {
     const charge = await stripe.charges.create({
       amount: 500,
